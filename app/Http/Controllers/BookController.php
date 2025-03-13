82214814f2d38439236f91af0f38a6487ae002e7
Farhaan-khan985 <faruskhan985@gmail.com>
2025-03-13 12:19:30 +0530

    farhan-book comit

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -7,7 +7,6 @@ use Illuminate\Http\Request;
 
 class BookController extends Controller
 {
-    // Show book list
     public function index()
     {
         $books = Book::all(); // Fetch all books
